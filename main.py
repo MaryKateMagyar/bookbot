9bbcfd58b7d9cd0371cccd6fe875c79e1f56af7b
MaryKateMagyar <marykatemagyar@gmail.com>
2025-03-11 21:50:43 -0700

    adjust main.py to be able to run various text files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import stats as s
+import sys
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -6,16 +7,16 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
-    book_location ="books/frankenstein.txt"
-    book_text = get_book_text("books/frankenstein.txt")
-    #book_location = "books/test.txt"
-    #book_text = get_book_text("books/test.txt")
-    #print(book_text)
+    book_location = sys.argv[1]
+    book_text = get_book_text(book_location)
     word_count = s.get_num_words(book_text)
-    #print(f"{word_count} words found in the document")
     character_count = s.get_character_count(book_text)
     dictionary = s.split_character_count(character_count)
     formatted_characters =s.format(book_location, word_count, dictionary)
     return formatted_characters
 
-main()
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    main()
