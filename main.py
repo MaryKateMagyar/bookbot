eaf8a703bc584a6e912530e742e5b8cf30fe98bb
MaryKateMagyar <marykatemagyar@gmail.com>
2025-03-11 19:32:35 -0700

    add sort_on and split_character_count to stats.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from stats import get_num_words
 from stats import get_character_count
+from stats import sorted_characters
+from stats import split_character_count
+from stats import sort_on
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -7,11 +10,16 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    #book_text = get_book_text("books/frankenstein.txt")
+    book_text = get_book_text("books/test.txt")
     #print(book_text)
     word_count = get_num_words(book_text)
-    print(f"{word_count} words found in the document")
+    #print(f"{word_count} words found in the document")
     character_count = get_character_count(book_text)
-    print(character_count)
+    #print(character_count)
+    dictionary = split_character_count(character_count)
+    #print(characters_sorted)
+    #dictionary.sort(reverse=True, key=sort_on)
+    print(dictionary)
 
 main()
--- stats.py
+++ stats.py
@@ -16,4 +16,25 @@ def get_character_count(book_text):
 
     return character_count
 
+def sort_on(dict):
+    return dict["count"]
 
+def split_character_count(character_count):
+    dictionary_list = []
+
+    for character in character_count:
+        if character.isalpha() == True:
+            dictionary = {}
+            number = character_count[character]
+            dictionary["character"] = character
+            dictionary["count"] = number
+            dictionary_list.append(dictionary)
+
+    dictionary_list.sort(reverse=True, key=sort_on)
+
+    return dictionary_list
+
+def sorted_characters(dictionary_list):
+    sorted = dictionary_list.sort(reverse=True, key=sort_on)
+
+    return sorted
