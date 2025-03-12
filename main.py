0205d16d3045876ae46f37e9db04160a55541d7a
MaryKateMagyar <marykatemagyar@gmail.com>
2025-03-11 22:49:49 -0700

    clean up pass on code, add notes to functions

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-import stats as s
+import stats
 import sys
-
-def get_book_text(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-    return file_contents
 
 def main():
     book_location = sys.argv[1]
-    book_text = get_book_text(book_location)
-    word_count = s.get_num_words(book_text)
-    character_count = s.get_character_count(book_text)
-    dictionary = s.split_character_count(character_count)
-    formatted_characters =s.format(book_location, word_count, dictionary)
-    return formatted_characters
+    book_text = stats.get_book_text(book_location)
+    num_words = stats.get_num_words(book_text)
+    character_count = stats.get_character_count(book_text)
+    dictionary = stats.sort_character_count(character_count)
 
-if len(sys.argv) < 2:
+    stats.format(book_location, num_words, dictionary)
+
+
+if len(sys.argv) != 2:
     print("Usage: python3 main.py <path_to_book>")
     sys.exit(1)
 else:
--- stats.py
+++ stats.py
@@ -1,9 +1,21 @@
+def get_book_text(path_to_file):
+    #converts the text of a file to a string to be compatible with other functions
+    try:
+        with open(path_to_file) as f:
+            book_text = f.read()
+        return book_text
+    except FileNotFoundError:
+        print(f"Error: {path_to_file} not found.")
+
 def get_num_words(book_text):
+    #gets the number of words in a string
     words = book_text.split()
     word_count = len(words)
     return word_count
 
 def get_character_count(book_text):
+    #creates a dictionary of all characters within a string in order of first appearance 
+    #and in a format of {"c1": number1, "c2": number2}
     all_lower = book_text.lower()
     character_count = {}
 
@@ -17,9 +29,12 @@ def get_character_count(book_text):
     return character_count
 
 def sort_on(dict):
+    #tells the sort method in sort_character_count to use the number of instances of a character as what to sort by
     return dict["count"]
 
-def split_character_count(character_count):
+def sort_character_count(character_count):
+    #Creates a list of dictionaries in the format of {"character": character, "count": number of times the character occurs}
+    #then sorts from most to least occurances
     dictionary_list = []
 
     for character in character_count:
@@ -34,11 +49,12 @@ def split_character_count(character_count):
 
     return dictionary_list
 
-def format(book_location, word_count, dictionary_list):
+def format(book_location, num_words, dictionary_list):
+    #outputs results of analysis in a pretty layout
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_location}...")
     print("----------- Word Count ----------")
-    print(f"Found {word_count} total words")
+    print(f"Found {num_words} total words")
     print("--------- Character Count -------")
     
     for dictionary in dictionary_list:
